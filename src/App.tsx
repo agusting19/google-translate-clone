650d56126c9b7159cd0abc48ebad8d934491a132
Agustin Gomez <agusleongomez@gmail.com>
2023-07-27 19:31:25 -0300

    added components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
 import { useStore } from "./hooks/useStore";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { AUTO_LENGUAGE } from "./constants";
 import "./App.css";
+import { ArrowsIcon } from "./components/icons";
+import LanguageSelector from "./components/LanguageSelector";
 
 function App() {
-  const { fromLanguage, setFromLanguage } = useStore();
+  const {
+    fromLanguage,
+    toLanguage,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+  } = useStore();
 
   return (
-    <div className="App">
+    <Container fluid>
       <h1>Google Translate</h1>
-      <button
-        onClick={() => {
-          setFromLanguage("es");
-        }}
-      >
-        Cambiar a español
-      </button>
-      <p>{fromLanguage}</p>
-    </div>
+      <Row>
+        <Col>
+          <LanguageSelector
+            type="from"
+            value={fromLanguage}
+            onChange={setFromLanguage}
+          />
+          {fromLanguage}
+        </Col>
+        <Col>
+          <Button
+            variant="link"
+            onClick={interchangeLanguages}
+            disabled={fromLanguage === AUTO_LENGUAGE}
+          >
+            <ArrowsIcon />
+          </Button>
+        </Col>
+        <Col>
+          <LanguageSelector
+            type="to"
+            value={toLanguage}
+            onChange={setToLanguage}
+          />
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
