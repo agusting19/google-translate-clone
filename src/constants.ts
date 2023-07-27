64baa2e7b604974da2c645eae5df1e5ee3550459
Agustin Gomez <agusleongomez@gmail.com>
2023-07-27 18:46:43 -0300

    added constants file

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const SUPORTED_LANGUAGES = {
+  en: "English",
+  es: "Español",
+  de: "Deutsch",
+  it: "Italiano",
+  pt: "Português",
+  ja: "日本語",
+  fr: "Français",
+};
+
+export const AUTO_LENGUAGE = "auto";
