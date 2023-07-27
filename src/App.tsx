78dbbb5c6dda7099a5e28f9acb943ac477b714f1
Agustin Gomez <agusleongomez@gmail.com>
2023-07-27 18:47:14 -0300

    added useStore hook to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-
+import { useStore } from "./hooks/useStore";
 import "./App.css";
 
 function App() {
+  const { fromLanguage, setFromLanguage } = useStore();
 
   return (
     <div className="App">
       <h1>Google Translate</h1>
+      <button
+        onClick={() => {
+          setFromLanguage("es");
+        }}
+      >
+        Cambiar a español
+      </button>
+      <p>{fromLanguage}</p>
     </div>
   );
 }
